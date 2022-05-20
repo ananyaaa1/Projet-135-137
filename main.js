ac79a432ae1c5dedc5e5ad171c2b54bb9cacae9d
ananyaaa1 <96527004+ananyaaa1@users.noreply.github.com>
2022-05-20 15:51:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+objects = [];
 video = "";
+Status = "";
 
 function preload()
 {
@@ -15,6 +17,36 @@ function setup()
 function draw()
 {
     image(video,0,0,480,380);
+    if(Status != "")
+    {
+        objectDetector.detect(video,gotResult);
+        for(i=0 ; i< objects.length ; i++)
+        {
+            document.getElementById("status").innerHTML="Object Detected";
+            document.getElementById("number_of_object").innerHTML="Number Of Objects" + objects.length;
+            fill("red");
+            percent= floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15, objects[i].y + 15);
+
+            noFill();
+            stroke("red");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }  
+    }
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+        objects = results;
+    }
+
 }
 
 function start()
